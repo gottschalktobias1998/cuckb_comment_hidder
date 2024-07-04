29636c833c3486232d0ecc52fa841bf76f74c04b
Tobias Gottschalk <gottschalk.tobias98@outlook.de>
2024-07-05 00:45:50 +0200

    first steps

---
+++ hider.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.card-footer');
+    elements.forEach(element => {
+        element.style.display = 'none';
+    });
+});
